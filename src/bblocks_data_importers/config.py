b55745ace294d96a062a7ae1c7af99e29c7797fb
Luca Picci <lpicci96@gmail.com>
2024-09-27 10:15:55 +0200

    add logging

---
+++ src/bblocks_data_importers/config.py
@@ -0,0 +1,29 @@
+"""Configuration for the data importers.
+
+Including:
+- Path configuration
+- Logger configuration
+
+TODO: raw data path settings
+TODO: cache settings
+TODO: error settings
+
+"""
+
+import logging
+
+# Configure Logging
+logger = logging.getLogger(__name__)
+shell_handler = logging.StreamHandler()  # Create terminal handler
+logger.setLevel(logging.INFO)  # Set levels for the logger, shell and file
+shell_handler.setLevel(logging.INFO)  # Set levels for the logger, shell and file
+
+# Format the outputs   "%(levelname)s (%(asctime)s): %(message)s"
+fmt_file = "%(levelname)s: %(message)s"
+
+# "%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
+fmt_shell = "%(levelname)s: %(message)s"
+
+shell_formatter = logging.Formatter(fmt_shell)  # Create formatters
+shell_handler.setFormatter(shell_formatter)  # Add formatters to handlers
+logger.addHandler(shell_handler)  # Add handlers to the logger
