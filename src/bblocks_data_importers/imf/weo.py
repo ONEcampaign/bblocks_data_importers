b1819bb839756e0bec13a72ad8c344191ee1eaed
Luca Picci <lpicci96@gmail.com>
2024-10-11 16:11:19 +0200

    add formatting steps

--- src/bblocks_data_importers/imf/weo.py
+++ src/bblocks_data_importers/imf/weo.py
@@ -7,7 +7,7 @@ from imf_reader import weo
 
 from bblocks_data_importers.protocols import DataImporter
 from bblocks_data_importers.utilities import convert_dtypes
-from bblocks_data_importers.config import logger
+from bblocks_data_importers.config import logger, weo_version, Fields
 
 
 class WEO(DataImporter):
@@ -17,19 +17,39 @@ class WEO(DataImporter):
         self._data: dict = {}
         self._latest_version = None
 
-    def _load_data(self, version = None) -> None:
+    @staticmethod
+    def _format_data(df: pd.DataFrame):
+        """Format WEO data"""
+
+        return (df
+                .pipe(convert_dtypes)
+                .rename(columns={"OBS_VALUE": Fields.value,
+                                 "TIME_PERIOD": Fields.year,
+                                 "REF_AREA_CODE": Fields.entity_code,
+                                 "REF_AREA_LABEL": Fields.entity_name,
+                                 "CONCEPT_CODE": Fields.indicator_code,
+                                 "CONCEPT_LABEL": Fields.indicator_name,
+                                 "UNIT_LABEL": Fields.unit,
+                                 "LASTACTUALDATE": "estimates_start_year",
+                                 })
+                # convert other columns to lowercase
+                .rename(columns={col: col.lower() for col in df.columns})
+                )
+
+    def _load_data(self, version=None) -> None:
         """Load WEO data to the object for a specific version
 
         Args:
             version: version of the WEO data to load. If None, the latest version is loaded
         """
 
-        self._data[weo.fetch_data.last_version_fetched] = weo.fetch_data(version)
+        self._data[weo.fetch_data.last_version_fetched] = weo.fetch_data(version).pipe(self._format_data)
 
+        # if the latest version is loaded, save the version to _latest_version
         if version is None:
             self._latest_version = weo.fetch_data.last_version_fetched
 
-    def get_data(self, version: Literal["latest"] | tuple[Literal['April', 'October'], int] = "latest") -> pd.DataFrame:
+    def get_data(self, version: weo_version = "latest") -> pd.DataFrame:
         """Get the WEO data for a specific version
 
         Args:
