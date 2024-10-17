98e6496b986b6441b169ea9934ecd343198405de
Jorge Rivera <jorge.rivera@one.org>
2024-10-17 09:12:03 +0200

    module structure and requirements
    Adds wbgapi to dependencies.
    Creates module structure

---
+++ src/bblocks_data_importers/world_bank/wb_api.py
@@ -0,0 +1,11 @@
+"""Importer for World Bank Data
+
+This importer provides functionality to easily get data from the World Bank databases.
+
+It is a wrapper for the wbgapi package, which provides an easy-to-use interface to the World Bank API.
+
+Usage:
+
+First instantiate an importer object:
+>>> wb = WorldBank()
+"""
