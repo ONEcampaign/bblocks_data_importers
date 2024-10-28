3a2529aba1c60b202442fcff1b4d098ae2053cd5
Luca Picci <lpicci96@gmail.com>
2024-10-28 12:36:48 +0100

    create function to get country info

--- src/bblocks_data_importers/wfp/wfp.py
+++ src/bblocks_data_importers/wfp/wfp.py
@@ -1,1 +1,31 @@
 """Module to get data from WFP"""
+
+
+import requests
+
+from bblocks_data_importers.config import (
+    logger,
+    DataExtractionError,
+    DataFormattingError,
+    Fields,
+)
+
+
+def get_country_ids() -> list[dict]:
+    """Get country ID, name, and iso3 code from WFP"""
+
+    url = "https://api.vam.wfp.org/dataviz/api/v1/ProxyEndpoints/GetExternalAPIResult"
+    params = {
+        "urlProxy": "https://api.vam.wfp.org/ReportsExplorer/v1/Countries",
+    }
+    headers = {
+        "referrer": "https://dataviz.vam.wfp.org/",
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        raise DataExtractionError(f"Error getting country IDs: {e}")
