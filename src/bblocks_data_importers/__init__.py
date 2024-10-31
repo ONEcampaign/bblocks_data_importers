c9f131435ac85a210866a1f28db80d0be4bb0b61
Luca Picci <lpicci96@gmail.com>
2024-10-31 17:20:16 +0100

    add object to init

--- src/bblocks_data_importers/__init__.py
+++ src/bblocks_data_importers/__init__.py
@@ -3,5 +3,6 @@ from importlib.metadata import version
 
 from bblocks_data_importers.who.ghed import GHED
 from bblocks_data_importers.imf.weo import WEO
+from bblocks_data_importers.wfp.wfp import WFPFoodSecurity
 
 __version__ = version("bblocks_data_importers")
