0e2e80bc61d7ee5906e756052effeaa2c1fbd987
Luca Picci <lpicci96@gmail.com>
2025-04-05 14:27:37 +0200

    create importer

--- src/bblocks_data_importers/config.py
+++ src/bblocks_data_importers/config.py
@@ -73,17 +73,16 @@ class Fields:
     value_upper = "value_upper"
     value_lower = "value_lower"
 
-    # country, region and other entity names
+    # country, region and other entity names and codes
     country_name = "country_name"
     region_name = "region_name"
-    entity_name = "entity_name"
-
-    # country, region and other entity codes
+    entity_name = "entity_name" # The name of the entity (country or region) to be used when entities are mixed
     iso2_code = "iso2_code"
     iso3_code = "iso3_code"
     entity_code = "entity_code"
     counterpart_code = "counterpart_code"
     counterpart_name = "counterpart_name"
+    region_code = "region_code"
 
     # time fields
     year = "year"
--- src/bblocks_data_importers/undp/hdi.py
+++ src/bblocks_data_importers/undp/hdi.py
@@ -3,8 +3,10 @@
 import pandas as pd
 import requests
 import io
+import numpy as np
 
 from bblocks_data_importers.config import logger, DataExtractionError, Fields
+from bblocks_data_importers.protocols import DataImporter
 
 
 DATA_URL = "https://hdr.undp.org/sites/default/files/2023-24_HDR/HDR23-24_Composite_indices_complete_time_series.csv"
@@ -81,4 +83,85 @@ def clean_metadata(metadata_df: pd.DataFrame) -> pd.DataFrame:
                         })
      )
 
+
+
+def clean_data(data_df: pd.DataFrame, metadata_df: pd.DataFrame) -> pd.DataFrame:
+    """ Clean the HDI data DataFrame.
+
+    Args:
+        data_df (pd.DataFrame): The HDI data DataFrame.
+        metadata_df (pd.DataFrame): The HDI metadata DataFrame.
+
+    Returns:
+        The cleaned HDI data DataFrame.
+    """
+
+    return (data_df
+            .rename(columns = {'iso3': Fields.entity_code, "country": Fields.entity_name, "region": Fields.region_code})
+            .melt(id_vars = [Fields.entity_code, Fields.entity_name, Fields.region_code], var_name = Fields.indicator_code, value_name = Fields.value)
+            .assign(split=lambda d: d.indicator_code.apply(lambda x: x.rsplit('_', 1) if '_' in x else [x, np.nan]))
+            .assign(indicator_code=lambda x: x['split'].str[0], year=lambda x: x['split'].str[1])
+            .drop(columns=['split'])
+            .assign(indicator_name= lambda d: d.indicator_code.map(metadata_df.set_index("indicator_code")['indicator_name'].to_dict()))
+            )
+
+
+class HumanDevelopmentIndex(DataImporter):
+    """A class to import Human Development Index (HDI) data from UNDP."""
+
+    def __init__(self, *, timeout: int = 30):
+        self._timeout = timeout
+        self._data_df: pd.DataFrame | None = None
+        self._metadata_df: pd.DataFrame | None = None
+
+
+    def _extract_metadata(self):
+        """Extract HDI metadata from the source."""
+
+        logger.info("Extracting HDI metadata")
+
+        metadata_df = read_hdi_metadata(timeout=self._timeout)
+        self._metadata_df = clean_metadata(metadata_df)
+
+    def _extract_data(self) -> None:
+        """Extract HDI data from the source."""
+
+        logger.info("Extracting HDI data")
+
+        df = read_hdi_data(timeout=self._timeout)
+        if self._metadata_df is None:
+            self._extract_metadata()
+
+        self._data_df = clean_data(df, self._metadata_df)
+
+
+    def get_metadata(self) -> pd.DataFrame:
+        """Get the HDI metadata
+
+        Returns:
+            The HDI metadata DataFrame.
+        """
+
+        if self._metadata_df is None:
+            self._extract_metadata()
+        return self._metadata_df
+
+    def get_data(self) -> pd.DataFrame:
+        """Get the HDI data
+
+        Returns:
+            The HDI data DataFrame.
+        """
+
+        if self._data_df is None:
+            self._extract_data()
+        return self._data_df
+
+    def clear_cache(self) -> None:
+        """Clear the cached data and metadata."""
+
+        self._data_df = None
+        self._metadata_df = None
+
+        logger.info("Cache cleared")
 
