c07a53afff47bd5c44c46873f51d4550b0ca6102
Luca Picci <lpicci96@gmail.com>
2025-12-04 16:12:08 +0100

    add wb init

---
+++ src/bblocks/data_importers/world_bank/__init__.py
@@ -0,0 +1,4 @@
+"""World Bank Data Importers"""
+
+from bblocks.data_importers.world_bank.world_bank import WorldBank, get_wb_databases, get_wb_entities, get_indicator_metadata, get_wb_indicators
+from bblocks.data_importers.world_bank.international_debt_statistics import InternationalDebtStatistics
