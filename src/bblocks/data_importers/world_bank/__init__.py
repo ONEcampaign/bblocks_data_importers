f474e29f579700dd009600e4a9fb3b13bb8f2d1b
Luca Picci <lpicci96@gmail.com>
2025-12-09 14:07:09 +0200

    update world bank imports

--- src/bblocks/data_importers/world_bank/__init__.py
+++ src/bblocks/data_importers/world_bank/__init__.py
@@ -4,8 +4,9 @@ from bblocks.data_importers.world_bank.world_bank import (
     WorldBank,
     get_wb_databases,
     get_wb_entities,
-    get_indicator_metadata,
+    get_wb_indicator_metadata,
     get_wb_indicators,
+    clear_wb_cache
 )
 from bblocks.data_importers.world_bank.international_debt_statistics import (
     InternationalDebtStatistics,
