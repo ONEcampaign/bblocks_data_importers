b94ce98174f16b75e060a2c8299dad0151d37b65
Luca Picci <lpicci96@gmail.com>
2025-12-12 19:04:10 +0200

    refactor changes

--- src/bblocks/data_importers/imf/dsa.py
+++ src/bblocks/data_importers/imf/dsa.py
@@ -5,16 +5,40 @@ from typing import Final
 import camelot
 import httpx
 import pandas as pd
-from bblocks.places import resolve_places
+
+from bblocks.data_importers.config import Fields, DataFormattingError
 
 URL: Final[str] = "https://www.imf.org/external/Pubs/ft/dsa/DSAlist.pdf"
 _FOOTNOTE_TRAILER = re.compile(r"\s*\d+/\s*$")
 
+# Columns
+COLS = {
+        1: Fields.country_name,
+        2: "latest_publication",
+        3: "risk_of_debt_distress",
+        5: "debt_sustainability_assessment",
+        6: "joint_with_world_bank",
+        7: "latest_dsa_discussed"
+    }
 
-def _strip_footnote_trailer(x: str | None) -> str | None:
+
+def __strip_footnote_trailer(x: str | None) -> str | None:
+    """Strip footnote trailer from string"""
+
     if not isinstance(x, str):
         return x
     return _FOOTNOTE_TRAILER.sub("", x).strip()
+
+def __normalise_country_names(df: pd.DataFrame) -> pd.DataFrame:
+    """Normalize country names"""
+
+    if Fields.country_name in df.columns:
+        df[Fields.country_name] = df[Fields.country_name].apply(__strip_footnote_trailer)
+
+    if df[Fields.country_name].isnull().any():
+        raise DataFormattingError("Null values found in country names after cleaning")
+
+    return df
 
 
 def _download_pdf(url: str) -> bytes:
@@ -47,15 +71,9 @@ def _pdf_to_df(src: bytes) -> pd.DataFrame:
 
 def __clean_headers(df: pd.DataFrame) -> pd.DataFrame:
     """Clean and standardize the DSA table headers."""
-    # strip whitespace and footnote trailers in all string cells
-    columns = {
-        1: "country",
-        2: "latest_publication",
-        3: "risk_of_debt_distress",
-        5: "debt_sustainability",
-        6: "joint_with_wb",
-    }
-    return df.filter(columns.keys()).rename(columns=columns)
+
+
+    return df.filter(COLS.keys()).rename(columns=COLS)
 
 
 def __normalise_booleans(df: pd.DataFrame, column: str) -> pd.DataFrame:
@@ -66,6 +84,8 @@ def __normalise_booleans(df: pd.DataFrame, column: str) -> pd.DataFrame:
 
 
 def __normalise_debt_distress(df: pd.DataFrame) -> pd.DataFrame:
+    """Normalise the "risk of debt distress" column"""
+
     df["risk_of_debt_distress"] = (
         df["risk_of_debt_distress"]
         .str.replace(r"\s+", " ", regex=True)
@@ -75,40 +95,62 @@ def __normalise_debt_distress(df: pd.DataFrame) -> pd.DataFrame:
         .str.replace("^moderate$", "Moderate", case=False, regex=True)
         .str.replace("^high$", "High", case=False, regex=True)
     )
+
+    # replace "" with NaN
+    df["risk_of_debt_distress"] = df["risk_of_debt_distress"].replace("", pd.NA)
+
     return df
 
 
 def __normalise_debt_sustainability(df: pd.DataFrame) -> pd.DataFrame:
-    df["debt_sustainability"] = (
-        df["debt_sustainability"]
-        .apply(_strip_footnote_trailer)
+    """Normalise the "debt sustainability assessment" column"""
+
+    df["debt_sustainability_assessment"] = (
+        df["debt_sustainability_assessment"]
+        .apply(__strip_footnote_trailer)
         .str.strip()
         .str.replace("^sustainable$", "Sustainable", case=False, regex=True)
         .str.replace("^unsustainable$", "Unsustainable", case=False, regex=True)
     )
 
+    # replace "" with NaN
+    df["debt_sustainability_assessment"] = df["debt_sustainability_assessment"].replace("", pd.NA)
+
     return df
 
 
 def __normalise_date(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Normalize date columns to datetime."""
+
     if column in df.columns:
         df[column] = pd.to_datetime(df[column], errors="coerce", format=None, utc=False)
     return df
 
 
-def __normalise_country_names(df: pd.DataFrame, column: str) -> pd.DataFrame:
-    if column in df.columns:
-        df[column] = df[column].apply(_strip_footnote_trailer)
-        df[column] = resolve_places(
-            df[column], to_type="name_short", not_found="ignore"
-        )
-    return df
+def _clean_df(df: pd.DataFrame) -> pd.DataFrame:
+    """Cleaning steps"""
+
+    try:
+
+        return (df.loc[lambda d: d[d.columns[0]].notna() & (d[d.columns[0]] != "") ]
+                .reset_index(drop=True)
+                .loc[1:, :] # drop header row
+                .pipe(__clean_headers)
+                 .pipe(__normalise_booleans, "joint_with_world_bank")
+                 .pipe(__normalise_country_names)
+                 .pipe(__normalise_date, "latest_publication")
+                 .pipe(__normalise_date, "latest_dsa_discussed")
+                 .pipe(__normalise_debt_distress)
+                 .pipe(__normalise_debt_sustainability)
+                .reset_index(drop=True)
+                )
+
+    except Exception as e:
+        raise DataFormattingError(f"Error cleaning DSA data: {str(e)}")
 
 
-def __insert_iso3_codes(df: pd.DataFrame) -> pd.DataFrame:
-    iso3_codes = resolve_places(df["country"], to_type="iso3_code", not_found="ignore")
-    df.insert(df.columns.get_loc("country"), "iso3_code", iso3_codes)
-    return df
+
+
 
 
 def get_dsa() -> pd.DataFrame:
@@ -126,19 +168,9 @@ def get_dsa() -> pd.DataFrame:
     """
 
     content = _download_pdf(url=URL)
-    data = _pdf_to_df(content)
-    return (
-        data.pipe(__clean_headers)
-        .pipe(__normalise_booleans, "joint_with_wb")
-        .pipe(__normalise_debt_distress)
-        .pipe(__normalise_debt_sustainability)
-        .pipe(__normalise_date, "latest_publication")
-        .pipe(__normalise_country_names, "country")
-        .pipe(__insert_iso3_codes)
-        .dropna(subset=["country"])
-        .reset_index(drop=True)
-    )
+    df = _pdf_to_df(content)
+    df = _clean_df(df)
+
+    return df
 
 
-if __name__ == "__main__":
-    df = get_dsa()
