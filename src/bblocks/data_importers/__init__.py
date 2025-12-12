eaed06dcdadf2b0a17e2ddba4388543482e64c0a
Luca Picci <lpicci96@gmail.com>
2025-12-12 19:38:00 +0200

    add get_dsa

--- src/bblocks/data_importers/__init__.py
+++ src/bblocks/data_importers/__init__.py
@@ -2,6 +2,7 @@ from importlib.metadata import version
 
 from bblocks.data_importers.who.ghed import GHED
 from bblocks.data_importers.imf.weo import WEO
+from bblocks.data_importers.imf.dsa import get_dsa
 from bblocks.data_importers.wfp.wfp import WFPFoodSecurity, WFPInflation
 from bblocks.data_importers.world_bank.wb_api import WorldBank
 from bblocks.data_importers.world_bank.ids import InternationalDebtStatistics
